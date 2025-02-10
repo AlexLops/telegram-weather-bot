5fce63f51bd6c5acd5fae7b80a9cd7a36da17d05
AlexLops <nirikom@gmail.com>
2025-02-10 21:12:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -26,4 +26,5 @@ def send_weather_to_telegram():
     return "Message Sent", 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=10000)
+
